52e91453a134874d8495fdbb6cd8179db8e0db52
Sven <kunze.sven93@web.de>
2016-02-22 10:39:28 +0100

    New Structure

---
+++ src/de/szut/ProjectZer0/model/testModel.java
@@ -0,0 +1,17 @@
+package de.szut.ProjectZer0.model;
+
+public class testModel {
+	private String testString;
+
+	public testModel()
+	{
+		testString = "Hallo Welt";
+	}
+	public String getTestString() {
+		return testString;
+	}
+
+	public void setTestString(String testString) {
+		this.testString = testString;
+	}
+}
