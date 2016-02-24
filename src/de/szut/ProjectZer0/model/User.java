9788fa8b70cb83b92f09b76f3e3b1637a4444f72
Sven <kunze.sven93@web.de>
2016-02-24 10:47:51 +0100

    new user model

--- src/de/szut/ProjectZer0/model/User.java
+++ src/de/szut/ProjectZer0/model/User.java
@@ -1,17 +1,36 @@
 package de.szut.ProjectZer0.model;
 
+@Entity
 public class User {
-	private String testString;
+	private Integer id;
+	private String username;
+	private String password;
+	
+	public User() {}
 
-	public User()
-	{
-		testString = "Hallo Welt";
-	}
-	public String getTestString() {
-		return testString;
+	public Integer getId() {
+		return id;
 	}
 
-	public void setTestString(String testString) {
-		this.testString = testString;
+	public void setId(Integer id) {
+		this.id = id;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
 }
