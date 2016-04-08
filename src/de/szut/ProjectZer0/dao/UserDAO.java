7da17af3e3af8bd2a59f30435c0cc7eec330b7e7
finn31 <finn.bohn@web.de>
2016-04-08 10:35:39 +0200

    changed structure
    added service for database access
    reworked daos

--- src/de/szut/ProjectZer0/dao/UserDAO.java
+++ src/de/szut/ProjectZer0/dao/UserDAO.java
@@ -4,10 +4,16 @@ import java.util.List;
 import de.szut.ProjectZer0.model.User;
 
 public interface UserDAO {
-	public void saveUser(User user);
+	/*public void saveUser(User user);
 	public List<User> listUser() ;
 	public User getUser(Integer id);
 	public void delUser(User user);
-	public List<User> getByName(String username);
+	public List<User> getByName(String username);*/
+	
+	User findById(int id);
+    void saveUser(User user);  
+    void deleteUserByUsername(String username);
+    List<User> getAllUser();
+    User findUserByUsername(String username);
 
 }
