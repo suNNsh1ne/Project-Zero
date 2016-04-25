a06ef84cfd5faabb50eee025b0c0144c39f02e64
finn31 <finn.bohn@web.de>
2016-04-25 09:10:34 +0200

    created Services + ServicesImpl
    created services +
    created servicesImpl
    NOT finished

---
+++ src/de/szut/ProjectZer0/service/LieferantService.java
@@ -0,0 +1,13 @@
+package de.szut.ProjectZer0.service;
+
+import java.util.List;
+
+import de.szut.ProjectZer0.model.User;
+
+public interface LieferantService {
+	User findById(int id);
+    void saveUser(User user);  
+    void deleteUserByUsername(String username);
+    List<User> getAllUser();
+    User findUserByUsername(String username);
+}
