9bd972c4f13585465a5a4ea9bef0f019c227cb20
finn31 <finn.bohn@web.de>
2016-04-28 13:49:13 +0200

    Service und DAO für Artikelstamm fertiggestellt
    Service und DAO für Artikelstamm fertiggestellt

--- src/de/szut/ProjectZer0/dao/ArtikelstammDAO.java
+++ src/de/szut/ProjectZer0/dao/ArtikelstammDAO.java
@@ -1,5 +1,14 @@
 package de.szut.ProjectZer0.dao;
 
+import java.util.List;
+
+import de.szut.ProjectZer0.model.Artikelstamm;
+
 public interface ArtikelstammDAO {
+	Artikelstamm findById(int id);
+    void saveArtikelstamm(Artikelstamm artikelstamm);  
+    void deleteArtikelstammByBezeichnung(String Bezeichnung);
+    List<Artikelstamm> getAllArtikelstamm();
+    Artikelstamm findArtikelstammByBezeichnung(String Bezeichnung);
 
 }
--- src/de/szut/ProjectZer0/service/ArtikelstammServiceImpl.java
+++ src/de/szut/ProjectZer0/service/ArtikelstammServiceImpl.java
@@ -1,5 +1,44 @@
 package de.szut.ProjectZer0.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import de.szut.ProjectZer0.dao.ArtikelstammDAO;
+import de.szut.ProjectZer0.model.Artikelstamm;
+
+@Service("ArtikelstammService")
+@Transactional
 public class ArtikelstammServiceImpl {
+    @Autowired
+    private ArtikelstammDAO dao;
+
+ 
+    public void saveArtikelstamm(Artikelstamm Artikelstamm) {
+        dao.saveArtikelstamm(Artikelstamm);
+    }
+ 
+    public void updateArtikelstamm(Artikelstamm Artikelstamm) {
+    	Artikelstamm entity = dao.findById(Artikelstamm.getArtikelstammId());
+        if(entity!=null){
+        	entity.setBezeichnung((Artikelstamm.getBezeichnung()));
+        }
+    }
+ 
+    public void deleteArtikelstammByBezeichnung(String Bezeichnung){
+        dao.deleteArtikelstammByBezeichnung(Bezeichnung);
+    }
+    
+ 
+    public List<Artikelstamm> getAllArtikelstamm() {
+    	return dao.getAllArtikelstamm();
+    }
+ 
+    public Artikelstamm findArtikelstammByBezeichnung(String Bezeichnung) {
+    	return dao.findArtikelstammByBezeichnung(Bezeichnung);
+    }
+
 
 }
