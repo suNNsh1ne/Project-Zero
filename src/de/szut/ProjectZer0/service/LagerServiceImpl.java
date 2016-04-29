4d78bb4baf4a342292f0d14ef035f487eaf417b7
finn31 <finn.bohn@web.de>
2016-04-29 10:32:07 +0200

    Implemented Service + DAO for Lager

--- src/de/szut/ProjectZer0/service/LagerServiceImpl.java
+++ src/de/szut/ProjectZer0/service/LagerServiceImpl.java
@@ -1,5 +1,48 @@
 package de.szut.ProjectZer0.service;
 
-public class LagerServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import de.szut.ProjectZer0.dao.LagerDAO;
+import de.szut.ProjectZer0.model.Lager;
+
+@Service("LagerService")
+@Transactional
+public class LagerServiceImpl implements LagerService {
+ 
+    @Autowired
+    private LagerDAO dao;
+
+ 
+    public void saveLager(Lager Lager) {
+        dao.saveLager(Lager);
+    }
+ 
+    public void updateLager(Lager Lager) {
+        Lager entity = dao.findById(Lager.getLagerID());
+        if(entity!=null){
+        	entity.setName(Lager.getName());
+        }
+    }
+ 
+    public void deleteLagerByName(String username){
+        dao.deleteLagerByName(username);
+    }
+    
+ 
+    public List<Lager> getAllLager() {
+    	return dao.getAllLager();
+    }
+ 
+    public Lager findLagerByName(String username) {
+    	return dao.findLagerByName(username);
+    }
+
+	@Override
+	public Lager findById(int id) {
+		return dao.findById(id);
+	}
 }
