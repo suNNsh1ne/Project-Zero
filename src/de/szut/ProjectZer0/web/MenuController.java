ce54e9d44aaddb7410d9c07213d9b4d19fd5d3e1
finn31 <finn.bohn@web.de>
2016-05-03 13:52:55 +0200

    implemented handling for mitarbeiter
    implemented handling for mitarbeiter (new + list)
    not finished

--- src/de/szut/ProjectZer0/web/MenuController.java
+++ src/de/szut/ProjectZer0/web/MenuController.java
@@ -3,6 +3,8 @@ package de.szut.ProjectZer0.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.szut.ProjectZer0.model.Artikelstamm;
+import de.szut.ProjectZer0.model.Mitarbeiter;
 import de.szut.ProjectZer0.model.User;
 import de.szut.ProjectZer0.service.ArtikelstammService;
+import de.szut.ProjectZer0.service.MitarbeiterService;
 import de.szut.ProjectZer0.service.UserService;
 
 // home.jsp erreichbar über Menü
@@ -23,6 +27,9 @@ public class MenuController {
     
 	//@Autowired
 	ArtikelstammService artikelstammService;
+	
+	@Autowired
+	MitarbeiterService mitarbeiterService;
 	
 	@RequestMapping("/home")
 	public String home() {
@@ -81,4 +88,56 @@ public class MenuController {
         //model.addAttribute("success", "Artikelstamm " + artikelstamm.getBezeichnung() + " registered successfully.");
         return "artikelstammList";
     }
+	
+	@RequestMapping(value = {"/artikelstammList"}, method = RequestMethod.GET)
+	public String listAllArtikelstamm(HttpServletRequest req, ModelMap model)
+	{
+		if(req.getSession().getAttribute("artikelstamm") != null)
+		{
+			List<Artikelstamm> artikelstamm = artikelstammService.getAllArtikelstamm();
+			model.addAttribute("Artikelstamm", artikelstamm);
+			return "artikelstammList";
+		}
+		else
+		{
+			return "error";
+		}
+	}
+	
+	@RequestMapping(value = {"/mitarbeiterNew"}, method = RequestMethod.GET)
+	public String mitarbeiterNew(ModelMap model) {
+		Mitarbeiter mitarbeiter = new Mitarbeiter();
+        model.addAttribute("Mitarbeiter", mitarbeiter);
+        model.addAttribute("edit", false);
+        return "mitarbeiterNew";
+	}
+	
+	@RequestMapping(value = { "/mitarbeiterNew" }, method = RequestMethod.POST)
+    public String saveMitarbeiter(Mitarbeiter mitarbeiter, BindingResult result,
+            ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            return "mitarbeiterNew";
+        }
+         
+        mitarbeiterService.saveMitarbeiter(mitarbeiter);
+ 
+        //model.addAttribute("success", "Mitarbeiter " + mitarbeiter.getBezeichnung() + " registered successfully.");
+        return "mitarbeiterList";
+    }
+	
+	@RequestMapping(value = {"/mitarbeiterList"}, method = RequestMethod.GET)
+	public String listAllMitarbeiter(HttpServletRequest req, ModelMap model)
+	{
+		if(req.getSession().getAttribute("mitarbeiter") != null)
+		{
+			List<Mitarbeiter> mitarbeiter = mitarbeiterService.getAllMitarbeiter();
+			model.addAttribute("Mitarbeiter", mitarbeiter);
+			return "mitarbeiterList";
+		}
+		else
+		{
+			return "error";
+		}
+	}
 }
