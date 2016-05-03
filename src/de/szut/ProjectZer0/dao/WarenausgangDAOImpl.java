9038e39e657d9f4592075db3b8bcee43ac3416fb
finn31 <finn.bohn@web.de>
2016-05-03 11:15:32 +0200

    Implemented Service + DAO for Warenausgang

--- src/de/szut/ProjectZer0/dao/WarenausgangDAOImpl.java
+++ src/de/szut/ProjectZer0/dao/WarenausgangDAOImpl.java
@@ -1,5 +1,23 @@
 package de.szut.ProjectZer0.dao;
 
-public class WarenausgangDAOImpl {
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.springframework.stereotype.Repository;
+import de.szut.ProjectZer0.model.Warenausgang;
+
+@Repository("WarenausgangDAO")
+public class WarenausgangDAOImpl extends AbstractDAO<Integer, Warenausgang> implements WarenausgangDAO{
+
+	@Override
+	public Warenausgang findById(int id) {
+		return getByKey(id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Warenausgang> getAllBestandsartikel() {
+		Criteria criteria = createEntityCriteria();
+        return (List<Warenausgang>) criteria.list();
+	}
 
 }
