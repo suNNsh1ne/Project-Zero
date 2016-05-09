2333256bc7e2c5f10439682ae09684fd10ca8ec1
finn31 <finn.bohn@web.de>
2016-05-09 11:14:01 +0200

    added mapping for every model
    added mapping for every model except user, wareneingang, -ausgang

--- src/de/szut/ProjectZer0/web/MenuController.java
+++ src/de/szut/ProjectZer0/web/MenuController.java
@@ -13,9 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import de.szut.ProjectZer0.model.Artikelstamm;
+import de.szut.ProjectZer0.model.Bestandsartikel;
+import de.szut.ProjectZer0.model.Lager;
+import de.szut.ProjectZer0.model.Lieferant;
 import de.szut.ProjectZer0.model.Mitarbeiter;
 import de.szut.ProjectZer0.model.User;
 import de.szut.ProjectZer0.service.ArtikelstammService;
+import de.szut.ProjectZer0.service.BestandsartikelService;
+import de.szut.ProjectZer0.service.LagerService;
+import de.szut.ProjectZer0.service.LieferantService;
 import de.szut.ProjectZer0.service.MitarbeiterService;
 import de.szut.ProjectZer0.service.UserService;
 
@@ -32,6 +38,15 @@ public class MenuController {
 	
 	@Autowired
 	MitarbeiterService mitarbeiterService;
+	
+	@Autowired
+	BestandsartikelService bestandsartikelService;
+	
+	@Autowired
+	LagerService lagerService;
+	
+	@Autowired
+	LieferantService lieferantService;
 	
 	@RequestMapping("/home")
 	public String home() {
@@ -84,6 +99,8 @@ public class MenuController {
 		return "lieferant";
 	}
 	
+	
+	
 	@RequestMapping(value = {"/artikelstammNew"}, method = RequestMethod.GET)
 	public String artikelstammNew(ModelMap model) {
 		Artikelstamm artikelstamm = new Artikelstamm();
@@ -121,6 +138,8 @@ public class MenuController {
 		}
 	}
 	
+	
+	
 	@RequestMapping(value = {"/mitarbeiterNew"}, method = RequestMethod.GET)
 	public String mitarbeiterNew(ModelMap model) {
 		Mitarbeiter mitarbeiter = new Mitarbeiter();
@@ -151,6 +170,123 @@ public class MenuController {
 			List<Mitarbeiter> mitarbeiter = mitarbeiterService.getAllMitarbeiter();
 			model.addAttribute("Mitarbeiter", mitarbeiter);
 			return "mitarbeiterList";
+		}
+		else
+		{
+			return "error";
+		}
+	}
+	
+	
+	
+	@RequestMapping(value = {"/bestandsartikelNew"}, method = RequestMethod.GET)
+	public String bestandsartikelNew(ModelMap model) {
+		Bestandsartikel bestandsartikel = new Bestandsartikel();
+        model.addAttribute("bestandsartikel", bestandsartikel);
+        model.addAttribute("edit", false);
+        return "bestandsartikelNew";
+	}
+	
+	@RequestMapping(value = { "/bestandsartikelNew" }, method = RequestMethod.POST)
+    public String saveBestandsartikel(Bestandsartikel bestandsartikel, BindingResult result,
+            ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            return "bestandsartikelNew";
+        }
+         
+        bestandsartikelService.saveBestandsartikel(bestandsartikel);
+ 
+        //model.addAttribute("success", "Bestandsartikel " + bestandsartikel.getBezeichnung() + " registered successfully.");
+        return "bestandsartikelList";
+    }
+	
+	@RequestMapping(value = {"/bestandsartikelList"}, method = RequestMethod.GET)
+	public String listAllBestandsartikel(HttpServletRequest req, ModelMap model)
+	{
+		if(req.getSession().getAttribute("bestandsartikel") != null)
+		{
+			List<Bestandsartikel> bestandsartikel = bestandsartikelService.getAllBestandsartikel();
+			model.addAttribute("Bestandsartikel", bestandsartikel);
+			return "bestandsartikelList";
+		}
+		else
+		{
+			return "error";
+		}
+	}
+	
+	
+	
+	@RequestMapping(value = {"/lagerNew"}, method = RequestMethod.GET)
+	public String lagerNew(ModelMap model) {
+		Lager lager = new Lager();
+        model.addAttribute("Lager", lager);
+        model.addAttribute("edit", false);
+        return "lagerNew";
+	}
+	
+	@RequestMapping(value = { "/lagerNew" }, method = RequestMethod.POST)
+    public String saveLager(Lager lager, BindingResult result,
+            ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            return "lagerNew";
+        }
+         
+        lagerService.saveLager(lager);
+ 
+        //model.addAttribute("success", "Lager " + lager.getBezeichnung() + " registered successfully.");
+        return "lagerList";
+    }
+	
+	@RequestMapping(value = {"/lagerList"}, method = RequestMethod.GET)
+	public String listAllLager(HttpServletRequest req, ModelMap model)
+	{
+		if(req.getSession().getAttribute("lager") != null)
+		{
+			List<Lager> lager = lagerService.getAllLager();
+			model.addAttribute("Lager", lager);
+			return "lagerList";
+		}
+		else
+		{
+			return "error";
+		}
+	}
+	
+	
+	
+	@RequestMapping(value = {"/lieferantNew"}, method = RequestMethod.GET)
+	public String lieferantNew(ModelMap model) {
+		Lieferant lieferant = new Lieferant();
+        model.addAttribute("lieferant", lieferant);
+        model.addAttribute("edit", false);
+        return "lieferantNew";
+	}
+	
+	@RequestMapping(value = { "/lieferantNew" }, method = RequestMethod.POST)
+    public String saveLieferant(Lieferant lieferant, BindingResult result,
+            ModelMap model) {
+ 
+        if (result.hasErrors()) {
+            return "lieferantNew";
+        }
+         
+        lieferantService.saveLieferant(lieferant);
+ 
+        //model.addAttribute("success", "Lieferant " + lieferant.getBezeichnung() + " registered successfully.");
+        return "lieferantList";
+    }
+	
+	@RequestMapping(value = {"/lieferantList"}, method = RequestMethod.GET)
+	public String listAllLieferant(HttpServletRequest req, ModelMap model)
+	{
+		if(req.getSession().getAttribute("lieferant") != null)
+		{
+			List<Lieferant> lieferant = lieferantService.getAllLieferant();
+			model.addAttribute("Lieferant", lieferant);
+			return "lieferantList";
 		}
 		else
 		{
