bb281b6e8b2d07bfc0b14f184a91d42aaf357074
Sven <kunze.sven93@web.de>
2016-05-09 19:36:07 +0200

    kleinigkeit

--- src/de/szut/ProjectZer0/config/HibernateInitializer.java
+++ src/de/szut/ProjectZer0/config/HibernateInitializer.java
@@ -1,9 +1,7 @@
 package de.szut.ProjectZer0.config;
 
 import java.util.Properties;
-
 import javax.sql.DataSource;
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
